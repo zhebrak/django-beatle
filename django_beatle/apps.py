3d0e113ecf685d36b95e23b7fe24580e736afe77
Alexander Zhebrak <fata2ex@gmail.com>
2017-01-19 18:22:47 +0300

    0.0.3

--- django_beatle/apps.py
+++ django_beatle/apps.py
@@ -1,5 +1,4 @@
-from django.apps import AppConfig
-from django.db.utils import ProgrammingError
+from django.apps import AppConfig, apps
 
 from .conf import settings
 
@@ -10,8 +9,7 @@ class BeatleConfig(AppConfig):
 
     def ready(self):
         try:
-            from .models import Task
-
+            Task = apps.get_model(app_label='django_beatle', model_name='Task')
             conf_tasks = settings.get_configuration().get('TASKS', [])
             db_tasks = dict(Task.objects.values_list('path', 'frequency'))
 
@@ -19,8 +17,5 @@ class BeatleConfig(AppConfig):
                 if path not in db_tasks:
                     task = Task(path=path, frequency=frequency)
                     task.save()
-
-        # no initial migration yet
-        except ProgrammingError:
+        except Exception:
             pass
-
